5f08f0dcaff8b31e791c1d370ae55357cca77073
RasmusEklind <eklind.r@gmail.com>
2016-01-28 15:52:39 +1100

    added functionality to about section.

--- partials/about.php
+++ partials/about.php
@@ -1,5 +1,8 @@
 <section class="section section--about">
 <div class="section__content section__content--about">
-  
+  <?php $post = get_post(9); ?>
+    <h2 class="section__heading"><?php echo $post->post_title; ?></h2>
+  <?php echo apply_filters('the_content', $post->post_content); ?>
+</div>
 </div>
 </section>
