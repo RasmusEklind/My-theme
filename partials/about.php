b099ee29fd6ea50fbc79d9b347c60d1b169ec9cd
RasmusEklind <eklind.r@gmail.com>
2016-02-18 15:11:16 +1100

    Added some random markup ^^,

--- partials/about.php
+++ partials/about.php
@@ -6,6 +6,17 @@
   <?php $post = get_post(9); ?>
     <h2 class="section__heading"><?php echo $post->post_title; ?></h2>
     <div class="section__heading--teaser">
+      <div class="featured__image--about">
+      <?php
+      $image_classes = 'article-teaser__featured article-teaser__featured-about';
+
+      if (has_post_thumbnail()) {
+        the_post_thumbnail('medium', array('class' => $image_classes));
+      } else {
+        echo wp_get_attachment_image(206, 'medium', false, array('class' => $image_classes));
+      }
+      ?>
+    </div>
       <?php echo apply_filters('the_content', $post->post_content); ?>
     </div>
 </div>
